fb9535705cd1f4ed8251c54a2fce0b596354af2e
MaheshtheDev <mahesh.svmr@gmail.com>
2022-05-14 22:53:15 +0530

    updated the ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ const Home: NextPage = () => {
       <h1 className="text-[#C0FF0D] font-semibold p-4">myFRS</h1>
       <main className="flex-grow"></main>
       <footer className="bottom-0 bg-[#161616] p-4 w-screen">
-        <nav className="list-none font-medium flex justify-around">
+        <nav className="list-none font-medium flex pb-4 justify-around">
           <li className="text-[#C0FF0D] px-2">Home</li>
           <li className="text-[#757575] px-2">Add Load</li>
           <li className="text-[#757575] px-2">Stats</li>
