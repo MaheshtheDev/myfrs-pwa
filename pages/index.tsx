fab54ff3fe9fd48dbc99b9bb93921db52dc1b02c
MaheshtheDev <mahesh.svmr@gmail.com>
2022-05-14 23:08:26 +0530

    add bottom nav bar

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,22 @@ const Home: NextPage = () => {
         <link rel="icon" href="/logo.svg" />
         <meta name="theme-color" content="#000000" />
       </Head>
-      <h1 className="text-[#C0FF0D] font-semibold p-4">myFRS</h1>
+      <h1 className="text-[#C0FF0D] font-semibold text-2xl p-4">myFRS</h1>
       <main className="flex-grow"></main>
       <footer className="bottom-0 bg-[#161616] p-4 w-screen">
-        <nav className="list-none font-medium flex pb-4 justify-around">
-          <li className="text-[#C0FF0D] px-2">Home</li>
-          <li className="text-[#757575] px-2">Add Load</li>
-          <li className="text-[#757575] px-2">Stats</li>
+        <nav className="list-none font-medium flex pb-4 justify-around text-xs">
+          <li className="text-[#C0FF0D] px-2 flex flex-col">
+            <Image src="/images/home.svg" height={35} width={35} />
+            <p className="pt-1">Home</p>
+          </li>
+          <li className="text-[#757575] px-2 flex flex-col">
+            <Image src="/images/add.svg" height={35} width={35} />
+            <p className="pt-1">Add Load</p>
+          </li>
+          <li className="text-[#757575] px-2 flex flex-col">
+            <Image src="/images/stats.svg" height={35} width={35} />
+            <p className="pt-1">Stats</p>
+          </li>
         </nav>
       </footer>
     </div>
